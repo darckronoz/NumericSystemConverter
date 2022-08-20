ca02d1a27be3f9c8dca68deab3ae78321753efde
darckronoz <crispipe14@gmail.com>
2022-08-19 19:56:59 -0500

    create the html, and js basic structure

---
+++ css/main.js
@@ -0,0 +1,7 @@
+const form = document.querySelector('#converter-main');
+const numN = document.querySelector('#numN');
+const numM = document.querySelector('#numM');
+const selectBaseN = document.querySelector('#baseN');
+const selectBaseM = document.querySelector('#baseM');
+
+selectBaseM.innerHTML = '<li> <i>1</i> </li>'
