9dee7d01eed3d076ca8422a062d2d4f1d6c9d6b7
darckronoz <crispipe14@gmail.com>
2022-08-19 22:57:40 -0500

    update the style file

--- main.js
+++ main.js
@@ -6,18 +6,28 @@ const selectBaseM = document.querySelector('#baseM');
 
 //fill both selects
 
-const options1 = [];
+const options = [];
+for(let i = 2; i < 37; i++) {
+    let optionN = document.createElement('option');
+    let v = i.toString();
+    optionN.innerHTML = v
+    optionN.setAttribute('value', v);
+    options.push(optionN);
+}
+
+for(n of options) {
+    selectBaseM.append(n);
+}
+
 const options2 = [];
 for(let i = 2; i < 37; i++) {
     let optionN = document.createElement('option');
     let v = i.toString();
     optionN.innerHTML = v
     optionN.setAttribute('value', v);
-    options1.push(optionN);
     options2.push(optionN);
 }
 
-for(let i = 0; i < options1.length; i++) {
-    selectBaseN.append(options1[i]);
-    selectBaseM.append(options2[i]);
+for(n of options2) {
+    selectBaseN.append(n);
 }
