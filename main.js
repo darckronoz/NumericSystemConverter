2b9c7087fab629d94d28a18f7726de039838e9e8
darckronoz <crispipe14@gmail.com>
2022-08-20 23:23:48 -0500

    added decimal to [2-9 ] base

--- main.js
+++ main.js
@@ -53,16 +53,16 @@ function fillLetterList() {
 
 fillLetterList();
 
-// convert from binary to dec *temporary*
+// convert from base [2 - 9] to dec *temporary*
 
-converBtn.addEventListener('click', convertBin);
+converBtn.addEventListener('click', convertDecToBase);
 
 function convertBin() {
     let n = numN.value;
     let x = selectBaseN.value;
     let y = selectBaseM.value;
     let answer = 0;
-    let exp = 0;
+    let exp = 0;    
 
     for(let i = n.length-1; i >= 0; i--) {
         answer += ((Math.pow(x, exp))*parseInt(n[i]));
@@ -70,5 +70,34 @@ function convertBin() {
     }
 
     resu.innerHTML = 'result: ' + answer.toString();
+}
+
+//convert from decimal to base [2-9]
+
+function convertDecToBase() {
+    let n = parseInt(numN.value);
+    let x = parseInt(selectBaseN.value);
+    let answer = [];
+    let counter = 0;
+    console.log('x: ', x);
+    console.log('n: ', n);
+
+    while(n >= x) {
+      answer.push(n%x);
+      n = Math.floor(n/x);
+      console.log('n: ', n);
+      console.log('answer: ', answer[counter]);
+      counter ++;
+    }
+    answer.push(n);
+    resu.innerHTML = 'result: ' + invertAnswer(answer);
+}
+
+function invertAnswer(a) {
+    let y = '';
+    while(a.length > 0) {
+        y += (a.pop());
+    }
+    return y;
 }
 
